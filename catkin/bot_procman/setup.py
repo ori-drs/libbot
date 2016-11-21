a82877d5b2ec2dac18b8a43f80ccece0a93e9b22
Vladimir Ivan <v.ivan@ed.ac.uk>
2016-11-21 16:08:51 +0000

    Added comments to catkin cmake Replaced python setup with cmake install

--- catkin/bot_procman/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['bot_procman'],
-    package_dir={'': '../../bot2-procman/python/src'}
-)
-
-setup(**d)
