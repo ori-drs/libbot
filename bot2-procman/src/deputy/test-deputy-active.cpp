e3d82fc8d5199156789669fee746f25bd77b39f5
Maurice Fallon <mauricefallon@gmail.com>
2019-02-28 20:26:30 +0000

    add host to print

--- bot2-procman/src/deputy/test-deputy-active.cpp
+++ bot2-procman/src/deputy/test-deputy-active.cpp
@@ -12,17 +12,17 @@ class Handler
     void handleMessage(const lcm::ReceiveBuffer* rbuf,
       const std::string& chan,
       const bot_procman::info2_t* msg){
-        printf("Received message on channel \"%s\"\n", chan.c_str());
+        printf("%s: Received message on channel \"%s\"\n", msg->host.c_str(), chan.c_str());
       }
     void handleMessage2(const lcm::ReceiveBuffer* rbuf,
       const std::string& chan,
       const bot_procman::orders2_t* msg){
-        printf("Received message on channel \"%s\"\n", chan.c_str());
+        printf("%s: Received message on channel \"%s\"\n", msg->host.c_str(), chan.c_str());
       }
     void handleMessage3(const lcm::ReceiveBuffer* rbuf,
       const std::string& chan,
       const bot_procman::discovery_t* msg){
-        printf("Received message on channel \"%s\"\n", chan.c_str());
+        printf("%s: Received message on channel \"%s\"\n", msg->host.c_str(), chan.c_str());
       }
 
 };
